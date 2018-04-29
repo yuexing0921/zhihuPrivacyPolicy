7b45679248a3de9ed9636db81946f025ee0b6a65
易全文 <yiquanwen@ezbuy.com>
2018-04-29 20:38:13 +0800

    增加知乎匹配范围

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-const _PrivacyInterval = setInterval(() => {
+window._PrivacyInterval = setInterval(() => {
   var modal = $(".Modal-wrapper .PrivacyConfirm-title");
   if (modal.length > 0) {
-    clearInterval(_PrivacyInterval);
-    modal.remove();
+    modal.closest(".Modal-wrapper").remove();
     var root = $("html");
     root.css("overflow", "auto");
+    clearInterval(window._PrivacyInterval);
   }
 }, 1000);
