3cc791cbe7efc0256f59a1f0e59c35f07369594c
易全文 <yiquanwen@ezbuy.com>
2018-04-29 20:28:14 +0800

    fix bug

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
-setInterval(() => {
-  var modal = $(".Modal-wrapper");
-  modal.remove();
-  var root = $("html");
-  root.css("overflow", "auto");
+const _PrivacyInterval = setInterval(() => {
+  var modal = $(".Modal-wrapper .PrivacyConfirm-title");
+  if (modal.length > 0) {
+    clearInterval(_PrivacyInterval);
+    modal.remove();
+    var root = $("html");
+    root.css("overflow", "auto");
+  }
 }, 1000);
